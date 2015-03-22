8b31d7acf9057bea1c3be9f04ba142bcfdeaf76e
lxg <lxg1986@gmail.com>
2015-03-22 20:30:55 +0800

    index request feixue.com

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    var sess = req.session
-    console.log(sess)
+    var sess = req.session;
+    req.request.get('http://feixue.com/api/cs_category?with_child=1',function(err,response,body){
+        console.log(err)
+        console.log(body)
+        res.render('index', { title: 'Express' });
+    })
 	var user = req.db.User
 	user.find(function(err,doc){
 		//console.log(doc,'doc')
 	})
-	res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
