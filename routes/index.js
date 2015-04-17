287bbf4b369de230e3f78c5c766477f0961c6e9a
lxg <lxg1986@gmail.com>
2015-04-17 17:21:38 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@ var express = require('express');
 var router = express.Router();
 var db = require('../server/db');
 var async = require('async');
+
+var request = require('request');
+var cookieRequest = function (userRequest, userResponse, url, callback) {
+    var options = {
+        url: url,
+        headers: {}
+    };
+    options.headers.Cookie = userRequest.header('Cookie'); // 将用户的 Cookie 传递给后台服务器
+    request(options, function (error, response, body) {
+        userResponse.setHeader('Cookie', response.headers.cookie);
+        callback.apply(null, arguments);
+    });
+};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var sess = req.session;
@@ -27,20 +42,21 @@ router.get('/', function(req, res, next) {
             resData.title = results.user.username;
             resData['user'] = results.user
             resData['user']['password'] = null
+            sess.user_id = results.user['_id']
         }
         resData.rooms = results.rooms;
-
+        
         res.render('index/index',resData)
     })
 });
+
 router.get('/about',function(req,res,next){
-    var session = req.session
-    console.log(req.header('Cookie'),'cccccccccccccccccccccccccccccc')
-    req.request.get('http://localhost/test.php',function(err,response,body){
-        console.log(session,'-------------------------')
-        console.log(body,'++++++++++++++++++')
+    //var session = req.session
+    req.session.user_id = Math.random()
+    cookieRequest(req,res,'http://localhost/test.php',function(err,response,body){
         res.render('index/about')
     })
+
 })
 
 module.exports = router;
